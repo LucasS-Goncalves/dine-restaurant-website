7d723a3b19ae86bc8591b3f952d7d3f683a23cae
Lucas <lucasgoncalves08@live.com>
2023-07-08 18:50:37 -0300

    working on benefits styles

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -13,24 +13,4 @@ import { Component } from '@angular/core';
 })
 export class HomepageComponent{
 
-  highlights = [
-    {
-      image1: '../../../assets/images/homepage/salmon-mobile@2x.jpg',
-      image2: '../../../assets/images/homepage/salmon-desktop-tablet@2x.jpg',
-      title: 'Seared Salmon Fillet',
-      description: 'Our locally sourced salmon served with a refreshing buckwheat summer salad.'
-    },
-    {
-      image1: '../../../assets/images/homepage/beef-mobile@2x.jpg',
-      image2: '../../../assets/images/homepage/beef-desktop-tablet@2x.jpg',
-      title: 'Rosemary Filet Mignon',
-      description: 'Our prime beef served to your taste with a delicious choice of seasonal sides.'
-    },
-    {
-      image1: '../../../assets/images/homepage/chocolate-mobile@2x.jpg',
-      image2: '../../../assets/images/homepage/chocolate-desktop-tablet@2x.jpg',
-      title: 'Summer Fruit Chocolate Mousse',
-      description: 'Creamy mousse combined with summer fruits and dark chocolate shavings.'
-    },
-  ]
 }
