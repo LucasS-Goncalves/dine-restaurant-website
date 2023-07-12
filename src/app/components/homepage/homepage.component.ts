54c316fab307ac1337bcfb1cde57d17b72bd9a38
Lucas <lucasgoncalves08@live.com>
2023-07-12 16:29:53 -0300

    improving styles and fixing bugs(header.css and benefits.css)

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -13,10 +13,59 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 })
 export class HomepageComponent{
 
-  @ViewChild('slideImg') slideImg!: ElementRef<HTMLImageElement>;
+  @ViewChild('mobileImg') mobileImg!: ElementRef<HTMLImageElement>;
+  @ViewChild('tabletImg') tabletImg!: ElementRef<HTMLImageElement>;
+  @ViewChild('desktopImg') desktopImg!: ElementRef<HTMLImageElement>;
+  @ViewChild('slideTitle') slideTitle!: ElementRef<HTMLTitleElement>;
+  @ViewChild('slideDescription') slideDescription!: ElementRef<HTMLParagraphElement>;
 
-  changeSlide(){
-    this.slideImg.nativeElement.src = "../../../assets/images/homepage/special-events-desktop@2x.jpg"
-    console.log('oi')
+  @ViewChild('slideOptions') slideOptions!: ElementRef<HTMLUListElement>;
+
+  changeSlide(slideOption: string, event: Event){
+
+    if(slideOption === 'family') {
+      const li = event.target as HTMLElement;
+      let activatedLi = this.slideOptions.nativeElement.querySelector('.activate');
+
+      this.mobileImg.nativeElement.srcset = "../../../assets/images/homepage/family-gathering-mobile@2x.jpg";
+      this.tabletImg.nativeElement.srcset = "../../../assets/images/homepage/family-gathering-tablet@2x.jpg";
+      this.desktopImg.nativeElement.srcset = "../../../assets/images/homepage/family-gathering-desktop@2x.jpg";
+
+      this.slideTitle.nativeElement.textContent = "Family Gathering";
+      this.slideDescription.nativeElement.textContent = "We love catering for entire families. So please bring everyone along for a special meal with your loved ones. We’ll provide a memorable experience for all."
+
+      activatedLi!.classList.remove('activate')
+      li.classList.add('activate');
+    }
+
+    if(slideOption === 'special') {
+      const li = event.target as HTMLElement;
+      let activatedLi = this.slideOptions.nativeElement.querySelector('.activate');
+
+      this.mobileImg.nativeElement.srcset = "../../../assets/images/homepage/special-events-mobile@2x.jpg";
+      this.tabletImg.nativeElement.srcset = "../../../assets/images/homepage/special-events-tablet@2x.jpg";
+      this.desktopImg.nativeElement.srcset = "../../../assets/images/homepage/special-events-desktop@2x.jpg";
+
+      this.slideTitle.nativeElement.textContent = "Special Events";
+      this.slideDescription.nativeElement.textContent = "Whether it’s a romantic dinner or special date you’re celebrating with others we’ll look after you. We’ll be sure to mark your special date with an unforgettable meal.";
+
+      activatedLi!.classList.remove('activate')
+      li.classList.add('activate');
+    }
+
+    if(slideOption === 'social') {
+      const li = event.target as HTMLElement;
+      let activatedLi = this.slideOptions.nativeElement.querySelector('.activate');
+
+      this.mobileImg.nativeElement.srcset = "../../../assets/images/homepage/social-events-mobile@2x.jpg";
+      this.tabletImg.nativeElement.srcset = "../../../assets/images/homepage/social-events-tablet@2x.jpg";
+      this.desktopImg.nativeElement.srcset = "../../../assets/images/homepage/social-events-desktop@2x.jpg";
+
+      this.slideTitle.nativeElement.textContent = "Social Events";
+      this.slideDescription.nativeElement.textContent = "Are you looking to have a larger social event? No problem! We’re more than happy to cater for big parties. We’ll work with you to make your event a hit with everyone.";
+
+      activatedLi!.classList.remove('activate')
+      li.classList.add('activate');
+    }
   }
 }
