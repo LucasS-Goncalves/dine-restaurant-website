bb3d98d94e164469740551bbdcf7d0e7b85cf1a7
Lucas <lucasgoncalves08@live.com>
2023-07-17 19:28:28 -0300

    working on form styles

--- src/app/components/booking/booking.component.ts
+++ src/app/components/booking/booking.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-booking',
   templateUrl: './booking.component.html',
   styleUrls: ['./booking.component.css']
 })
-export class BookingComponent {
+export class BookingComponent implements OnInit{
 
+  thisYear = new Date().getFullYear()
+
+  ngOnInit(): void {
+    console.log(this.thisYear)
+  }
 }
